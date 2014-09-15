10700faed780697e46ed7631bc4f20c5bb7b7398
Sam Couch <samcouch91@gmail.com>
2014-09-14 22:35:22 -0400

    Add assignment 2

---
+++ assignment-2/src/Intcoll4.java
@@ -0,0 +1,143 @@
+//*********************************************************************
+// FILE NAME    : Intcoll4.java
+// DESCRIPTION  : This file contains the class Intcoll4.
+// NAME  : Sam Couch - couch@temple.edu
+//*********************************************************************
+
+import java.util.*;
+
+public class Intcoll4
+{
+    private class ListNode
+    {
+        private int info;
+        private ListNode link;
+        
+        public ListNode()
+        {
+            //initialize info block to 0, link to null (none)
+            info=0; link=null;
+        }
+        
+        public ListNode(int i, ListNode l)
+        {
+            //initialize info block to i, link to l (another node)
+            info=i; link=l;
+        }
+    }
+    private ListNode c;
+    private int how_many;
+
+    // Initializes Intcoll4 instance which can hold 500 ints
+    public Intcoll4()
+    {
+        c = null;
+        how_many = 0;
+    }
+
+    // Initializes Intcoll4 instance which can hold i ints
+    public Intcoll4(int i)
+    {
+        c = null;
+        how_many = 0;
+    }
+
+    // Copys contents of Intcoll4 instance named obj to this
+    public void copy(Intcoll4 obj)
+    {
+        if (this != obj)
+        {
+            c = null;
+            ListNode j = obj.c, k = null, t = new ListNode();
+            while (j != null)
+            {
+                t.info = j.info;
+                if(k != null) k.link = t;
+                else c = t;
+                k = t;
+                t = new ListNode();
+                j = j.link;
+            }
+            how_many = obj.how_many;
+        }
+    }
+
+    // Returns true if collection contains int i, false if otherwise
+    public boolean belongs(int i)
+    {
+        ListNode j = c;
+        while ((j != null)&&(j.info != i)){ 
+            j = j.link;
+        }
+        return (j != null);
+    }
+
+    // Inserts i into collection. If collection already contains i, function returns
+    // if collection is filled to capacity, capacity is doubled
+    public void insert(int i)
+    {
+        if (i > 0)
+        {
+            ListNode j = c;
+            while ((j != null) && (j.info != i)){ 
+                j=j.link;
+            }
+            if (j == null)
+            {
+                j = new ListNode(i, c);
+                c = j;
+                how_many++;
+            }
+        }
+    }
+
+    // If collection contains i, i will be omitted
+    public void omit(int i)
+    {
+        ListNode j = c,p = null;
+        while ((j != null)&&(j.info != i))
+        {
+            p=j; j=j.link;
+        }
+        if (j != null)
+        {
+            how_many--;
+            if (p == null) {
+                c = j.link;
+            } else {
+                p.link = j.link;
+            }
+        }
+    }
+
+    // Returns amount of ints stored in collection
+    public int get_howmany()
+    {
+        return how_many;
+    }
+
+    // Prints contents of collection to output
+    public void print()
+    {
+        ListNode j = c;
+        System.out.println();
+        while (j != null)
+        {
+            System.out.println(j.info); j = j.link;
+        }
+    }
+
+    // returns true if both Intcoll4 instances contain
+    // identical int collections
+    public boolean equals(Intcoll4 obj)
+    {
+        ListNode j = c;
+        boolean result = how_many == obj.how_many;
+        while ((j != null)&&result)
+        {
+            result = obj.belongs(j.info); j = j.link;
+        }
+        return result;
+    }
+}
+
---
+++ assignment-2/src/Intcoll4client.java
@@ -0,0 +1,40 @@
+
+//**********************************************************
+// FILE: NAME   : intcoll1client.java
+// DESCRIPTION  : This is a client of class Intcoll4.
+//**********************************************************
+
+import java.util.*;
+
+public class Intcoll4client
+{
+    public static final int SENTINEL = 0;
+
+    public static void main(String[] args)
+    {
+        int value; Scanner keyboard=new Scanner(System.in);
+        Intcoll4 P=new Intcoll4(), N=new Intcoll4(), L= new Intcoll4();
+
+        System.out.println("Enter an integer to be inserted or 0 to quit:");
+        value=keyboard.nextInt();
+        while(value != SENTINEL)
+        {
+            if (value > 0) {P.insert(value); L.insert(value);}
+            else {N.insert(-value); L.omit(-value);}
+            System.out.println("Enter next integer to be inserted or 0 to quit:");
+            value=keyboard.nextInt();
+        }
+        System.out.println("\nThe values in collection P are:");
+        P.print();
+        System.out.println("\nThe values in collection N are:");
+        N.print();
+        System.out.println("\nThe values in collection L are:");
+        L.print();
+        if (P.equals(N)) System.out.println("\nP and N are equal.");
+        else System.out.println("\nP and N are NOT equal.");
+        Intcoll4 A=new Intcoll4(); A.copy(L);
+        System.out.println("\nThe values in the copy of L are:\n");
+        A.print();
+    }
+}
+
